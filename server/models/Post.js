e0ebb553e1fa252e1733e3b6553e6aa978898d97
Bryson Kern <kernbryson@yahoo.com>
2022-08-05 14:14:19 -0400

    started on profile page

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,10 +1,10 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const postSchema = new Schema({
   postText: {
     type: String,
-    required: 'You need to leave a post!',
+    required: "You need to leave a post!",
     minlength: 1,
     maxlength: 280,
     trim: true,
@@ -13,6 +13,28 @@ const postSchema = new Schema({
     type: String,
     required: true,
     trim: true,
+  },
+
+  volunteerDate: {
+    type: String,
+  
+  },
+  location: {
+    type: String,
+    
+  },
+  time: {
+    type: String,
+   
+  },
+  contact: {
+    type: String,
+   
+  },
+  category: {
+    type: Schema.Types.ObjectId,
+    ref: "Category",
+   
   },
   createdAt: {
     type: Date,
@@ -40,6 +62,6 @@ const postSchema = new Schema({
   ],
 });
 
-const Post = model('Post', postSchema);
+const Post = model("Post", postSchema);
 
 module.exports = Post;

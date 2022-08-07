b4f606c114ce55cbe0691a998dcd040d021c5096
Bryson Kern <kernbryson@yahoo.com>
2022-08-07 13:17:15 -0400

    updated new homepage

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { Grid } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+
 const PostList = ({ posts, title, showTitle = true, showUsername = true }) => {
   if (!posts.length) {
     return <h3>No Posts Yet</h3>;
@@ -10,42 +16,54 @@ const PostList = ({ posts, title, showTitle = true, showUsername = true }) => {
       {showTitle && <h3>{title}</h3>}
       {posts &&
         posts.map((post) => (
-          <div key={post._id} className="card mb-3">
-            <h4 className="card-header bg-primary text-light p-2 m-0">
-              {showUsername ? (
-                <Link
-                  className="text-light"
-                  to={`/profiles/${post.postAuthor}`}
-                >
-                  {post.postAuthor} <br />
-                  <span style={{ fontSize: "1rem" }}>
-                    created this post on {post.createdAt}
-                  </span>
-                </Link>
-              ) : (
-                <>
-                  <span style={{ fontSize: "1rem" }}>
-                    You created this post on {post.createdAt}
-                  </span>
-                </>
-              )}
-            </h4>
-            <div className="card-body bg-light p-2">
-              <p>{post.postText}</p>
-              <p>{post.location}</p>
-              <p>{post.category}</p>
-              <p>{post.time}</p>
-              <p>{post.contact}</p>
-              <p>{post.volunteerDate}</p>
-            </div>
-            <Link
-              className="btn btn-primary btn-block btn-squared"
-              to={`/posts/${post._id}`}
-            >
-              Join the discussion on this post.
-            </Link>
-          </div>
+          <Grid key={post._id} container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography variant="h5">
+                    {" "}
+                    {showUsername ? (
+                      <Link
+                        className="text-light"
+                        to={`/profiles/${post.postAuthor}`}
+                      >
+                        {post.postAuthor} <br />
+                        <span style={{ fontSize: "1rem" }}>
+                          created this post on {post.createdAt}
+                        </span>
+                      </Link>
+                    ) : (
+                      <>
+                        <span style={{ fontSize: "1rem" }}>
+                          You created this post on {post.createdAt}
+                        </span>
+                      </>
+                    )}
+                    ;
+                  </Typography>
+
+                  <Typography variant="body2">
+                    <p>{post.postText}</p>
+                    <p>{post.location}</p>
+                    <p>{post.category}</p>
+                    <p>{post.time}</p>
+                    <p>{post.contact}</p>
+                    <p>{post.volunteerDate}</p>
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Link
+                    className="btn btn-primary btn-block btn-squared"
+                    to={`/posts/${post._id}`}
+                  >
+                    Join the discussion on this post.
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
         ))}
+      ;
     </div>
   );
 };
--- client/src/pages/Home2.js
+++ client/src/pages/Home2.js
@@ -1,137 +1,71 @@
 import React from "react";
-import { Grid, Link } from "@mui/material";
-import Box from "@mui/material/Box";
+import { Link } from "react-router-dom";
+
+import { Grid } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
-import { useQuery } from "@apollo/client";
 
-import PostList from "../components/PostList";
-import PostForm from "../components/PostForm";
-
-const Home2 = () => {
+const PostList = ({ posts, title, showTitle = true, showUsername = true }) => {
+  if (!posts.length) {
+    return <h3>No Posts Yet</h3>;
+  }
   return (
     <div>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6} lg={4}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5">Plant trees in Oakhurst</Typography>
+      {showTitle && <h3>{title}</h3>}
+      {posts &&
+        posts.map((post) => (
+          <Grid key={post._id} container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                  <Typography variant="h5">
+                    {" "}
+                    {showUsername ? (
+                      <Link
+                        className="text-light"
+                        to={`/profiles/${post.postAuthor}`}
+                      >
+                        {post.postAuthor} <br />
+                        <span style={{ fontSize: "1rem" }}>
+                          created this post on {post.createdAt}
+                        </span>
+                      </Link>
+                    ) : (
+                      <>
+                        <span style={{ fontSize: "1rem" }}>
+                          You created this post on {post.createdAt}
+                        </span>
+                      </>
+                    )}
+                    ;
+                  </Typography>
 
-              <Typography variant="body2">
-                We need 5-10 people this Saturday to come help plant trees
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Link href="/singlepost2">
-                <Button size="small">Learn More</Button>
-              </Link>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5">Rescue Greyhound Shelter!</Typography>
-
-              <Typography variant="body2">
-                We are always looking for people. Monday through Friday during
-                the day!
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5">Habitat for Humanity</Typography>
-
-              <Typography variant="body2">
-                Come help and learn how to build a house!
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5">Elder Care</Typography>
-
-              <Typography variant="body2">
-                Spend time with someone. It will make you feel great!
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5">After School Program</Typography>
-
-              <Typography variant="body2">
-                Tutors needed for grades k-12 after school program in the
-                Atlanta area.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-              <Typography variant="h5">Atlanta Food Bank</Typography>
-
-              <Typography variant="body2">
-                Need food prep people as well as drivers. Groups welcome.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
+                  <Typography variant="body2">
+                    <p>{post.postText}</p>
+                    <p>{post.location}</p>
+                    <p>{post.category}</p>
+                    <p>{post.time}</p>
+                    <p>{post.contact}</p>
+                    <p>{post.volunteerDate}</p>
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Link
+                    className="btn btn-primary btn-block btn-squared"
+                    to={`/posts/${post._id}`}
+                  >
+                    Join the discussion on this post.
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
+          </Grid>
+        ))}
+      ;
     </div>
   );
 };
 
-// import { QUERY_POSTS } from '../utils/queries';
-
-// const Home = () => {
-//   const { loading, data } = useQuery(QUERY_POSTS);
-//   const posts = data?.posts || [];
-
-//   return (
-//     <main>
-//       <div className="flex-row justify-center">
-
-//         <div className="col-12 col-md-8 mb-3">
-//           {loading ? (
-//             <div>Loading...</div>
-//           ) : (
-//             <PostList
-//               posts={posts}
-//               title="Some Feed for Post(s)..."
-//             />
-//           )}
-//         </div>
-//       </div>
-//     </main>
-//   );
-// };
-
-export default Home2;
+export default PostList;

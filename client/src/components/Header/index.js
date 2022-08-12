35193a8e507cd672e18885008f3c769e70cd8438
Bryson Kern <kernbryson@yahoo.com>
2022-08-11 22:15:41 -0400

    delete dupe

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -8,9 +8,7 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
-
 import Button from "@mui/material/Button";
-import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 
 import Auth from "../../utils/auth";
